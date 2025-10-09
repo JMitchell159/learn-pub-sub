2e8f51c83078f14f864ae39665e338c808647b72
JMichell159 <joemitchell159@gmail.com>
2025-10-09 06:16:20 -0500

    added PublishJSON function & server implements it

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,11 +17,23 @@ func main() {
 	connString := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connString)
 	if err != nil {
-		log.Printf("something went wrong while creating a connection: %v", err)
+		log.Printf("error while creating a connection: %v", err)
 	}
 	defer conn.Close()
 
 	fmt.Println("Connection successful!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Printf("error while creating a channel from connection: %v", err)
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Printf("error while publishing paused state: %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
